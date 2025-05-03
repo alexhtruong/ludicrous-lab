49afe31a56e97368f587384e29a17f65b1321d66
Alex Truong <46734496+alexhtruong@users.noreply.github.com>
2025-05-02 22:39:13 -0700

    bug fix

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -58,8 +58,6 @@ def post_deliver_bottles(potions_delivered: List[PotionMixes], order_id: int):
                 SELECT 1 
                 FROM (
                     SELECT order_id FROM potion_ledger WHERE order_id = :order_id
-                    UNION 
-                    SELECT order_id FROM liquid_ledger WHERE order_id = :order_id
                 ) AS combined_ledgers
                 """
             ),
@@ -69,7 +67,7 @@ def post_deliver_bottles(potions_delivered: List[PotionMixes], order_id: int):
         ).first()
 
         if existing_order:
-            print("ORDER ALREADY EXISTS IN EITHER POTION OR LIQUID LEDGER")
+            print("ORDER ALREADY EXISTS IN EITHER POTION")
             return
         
         ml_used = {"red_ml": 0, "green_ml": 0, "blue_ml": 0, "dark_ml": 0}
